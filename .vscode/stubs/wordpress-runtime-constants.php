7145e42f81921ceeb3d0f81d02934f7a9b50e6c9
joeseverino <github@jseverino.com>
2026-04-25 11:22:28 -0500

    Add local WordPress PHP analysis tooling

---
+++ .vscode/stubs/wordpress-runtime-constants.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    define('ABSPATH', '/wordpress/');
+}
+
+if (!defined('MINUTE_IN_SECONDS')) {
+    define('MINUTE_IN_SECONDS', 60);
+}
+
+if (!defined('HOUR_IN_SECONDS')) {
+    define('HOUR_IN_SECONDS', 3600);
+}
+
+if (!defined('DAY_IN_SECONDS')) {
+    define('DAY_IN_SECONDS', 86400);
+}
